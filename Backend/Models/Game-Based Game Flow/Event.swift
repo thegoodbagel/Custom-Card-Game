7bc18e5a18863b946611b6e90907911ca8cd12ab
Abrianna <abrianna@mit.edu>
2025-06-23 21:21:24 -0700

    deleted old folder

--- Backend/Models/Game-Based Game Flow/Event.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Event.swift
-//  Card Game
-//
-//  Created by Abrianna Zhang on 6/17/25.
-//
-
-import Foundation
-// Allows multiple players to make moves at the same time
--- Backend/Models/Rule-Based Free Flow/Rule.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Rule.swift
-//  Card Game
-//
-//  Created by Abrianna Zhang on 6/17/25.
-//
-
-import Foundation
