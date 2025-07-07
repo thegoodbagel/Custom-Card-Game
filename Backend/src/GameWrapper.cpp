efab99b164e545b6f4c7a9baa218477146461794
Abrianna <abrianna@mit.edu>
2025-07-07 09:39:03 -0700

    update state to include play pile

--- Backend/src/GameWrapper.cpp
+++ Backend/src/GameWrapper.cpp
@@ -26,19 +26,6 @@ void GameWrapper::AddPlayer(const Napi::CallbackInfo& info) {
 
 // One argument: string of player ID (socket ID)
 // Returns the game state JSON string from the perspective of the player
-// Example: 
-// {
-//   "drawPileSize": 42,
-//   "hand": [
-//     {"id": "3", "value": "4", "location": "hand", "face": "up"},
-//     {"id": "7", "value": "8", "location": "hand", "face": "up"}
-//     // ... cards in player's hand
-//   ],
-//   "otherPlayers": {
-//     "playerId_2": {"handSize": 5},
-//     "playerId_3": {"handSize": 3}
-//   }
-// }
 Napi::Value GameWrapper::GetState(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
     std::string playerId = info[0].As<Napi::String>();
--- Backend/src/SimpleGame.cpp
+++ Backend/src/SimpleGame.cpp
@@ -11,32 +11,16 @@ SimpleGame::SimpleGame() {
 
 // One argument: string of player ID (socket ID)
 // Returns the game state JSON string from the perspective of the player
-// Example: 
-// {
-//   "drawPileSize": 42,
-//   "hand": [
-//     {"id": "3", "value": "4", "location": "hand", "face": "up"},
-//     {"id": "7", "value": "8", "location": "hand", "face": "up"}
-//     // ... cards in player's hand
-//   ],
-//   "otherPlayers": {
-//     "playerId_2": {"handSize": 5},
-//     "playerId_3": {"handSize": 3}
-//   }
-// }
+
 std::string SimpleGame::getState(std::string playerID) {
-    // For now, there are only three possible locations:
-    // Cards on table (draw pile),
-    // Cards in players' hands,
-    // Cards in other player's hands (invisible for now)
-    // Send over the appropriate state of the game
     nlohmann::json state;
 
     state["drawPileSize"] = drawPile.size();
 
     // Add own hand
     state["hand"] = players[playerID]->getHand();
-    std::cout << "Getting hand of player " << playerID << std::endl;
+
+    state["playPile"] = playPile;
 
     // Other players: only hand sizes
     nlohmann::json others;
